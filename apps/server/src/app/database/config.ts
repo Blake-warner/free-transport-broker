d2c6a1b87d8bf16e078b175701dd78d5f2723bef
Blake Warner <blakewarner23@gmail.com>
2025-01-31 03:17:34 -0800

    fixed rendering issues with config envi vars.

--- apps/server/src/app/database/config.ts
+++ apps/server/src/app/database/config.ts
@@ -1,10 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('db', () => ({
-  type: process.env.DATABASE_TYPE as "mysql",
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  name: process.env.DATABASE_NAME,
+  type: process.env.DATABASE_TYPE || "mysql",
+  host: process.env.DATABASE_HOST || "localhost",
+  port: Number(process.env.DATABASE_PORT) || 3306,
+  username: process.env.DATABASE_USER_NAME || "root",
+  password: process.env.DATABASE_PASSWORD || "root",
+  name: process.env.DATABASE_NAME || "ftb",
 }));
